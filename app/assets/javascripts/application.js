fb400c1b209fdda2f05f3d8edaa044ae6fcd5cdd
bono <bono.lau@gmail.com>
2015-11-24 14:11:54 +1100

    24/11 1400

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore
 //= require_tree .
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
    
-   root 'pages#home'
-
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+   root 'pages#home'
 
   namespace :api do
     resources :categories, only: [:index]
