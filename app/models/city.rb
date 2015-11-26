2ac8da9415fcf33b25326fd94c5e8f5cff72f7ff
Aviel Goh <avielgwk@gmail.com>
2015-11-26 15:02:02 +1100

    create world map using D3

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,10 +5,16 @@ class City < ActiveRecord::Base
   API_KEY = "key=682973467622505a78213a1670961"
 
   def self.init_cities
-    cities = HTTParty.get("#{ BASE_URL }/2/cities.json/?&page=200&radius=9999999999&only=city,country,member_count&#{ API_KEY }")
+    citiesFirst = HTTParty.get("#{ BASE_URL }/2/cities.json/?&page=200&radius=9999999999&#{ API_KEY }")
 
-    cities.first[1].each do |city|
-      City.create(name: "#{city['city']}", country: "#{city['country']}", member_count: city['member_count'])
+    citiesFirst.first[1].each do |city|
+      City.create(name: "#{city['city']}", country: "#{city['country']}", member_count: city['member_count'], lon: city['lon'], lat: city['lat'])
+    end
+
+    citiesSecond = HTTParty.get("#{ BASE_URL }/2/cities.json/?&page=200&offset=1&radius=9999999999&#{ API_KEY }")
+
+    citiesSecond.first[1].each do |city|
+      City.create(name: "#{city['city']}", country: "#{city['country']}", member_count: city['member_count'], lon: city['lon'], lat: city['lat'])
     end
   end
 
